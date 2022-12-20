c87511e1c981ae87af5b1edc39aeb04d246eabdc
SahilChowkekar <chowkekar95@gmail.com>
2022-12-19 19:24:31 -0800

    Implementation of Depth First Search Traversal

--- search.py
+++ search.py
@@ -73,9 +73,66 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
+def depthFirstSearch(problem):
+    """
+    Search the deepest sns in the search tree first.
+
+    # Your search algorithm needs to return a list of actions that reaches the
+    # goal. Make sure to implement a graph search algorithm.
+
+    # To get started, you might want to try some of these simple commands to
+    # understand the search problem that is being passed in:
+
+    # print "Start:", problem.getStartState()
+    # print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    # print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    "*** YOUR CODE HERE ***"
+
+    #Frontier= F
+    #current= It stores the path
+    #v= It have the value of set of visited nodes
+    #s= sucessor
+    #cs= childnode
+    #nw= newdirection
+    #sn=startnode
+    #In dfs, there is stack is used to store where set and array is used for storing visited nodes and path. Depending upon that it check
+    # all the the condition first and then depends upon condition it returns or add the new path till it reaches the goal stage.
+
+
+    F = util.Stack()
+    current = [] #here, store the search route from the start node to the destination.
+    v =set() #a collection of visited nodes
+    sn =problem.getStartState()
+    F.push((sn,current)) #Incorporate the route and the node into the stack.
+
+    flag= True
+    while flag:
+        if F.isEmpty():
+            return []
+        #When I remove a stack item, I take the node and add it to the visited set.
+        sn,current=F.pop()
+        v.add(sn)
+        if problem.isGoalState(sn): # Return the search path if the goal has been reached.
+            return current
+        s=problem.getSuccessors(sn) #take the successors of the current node/state
+        for i in s:  # i[0]:next_state  i[1]:action  i[2]:cost
+            cs =i[0]
+            if cs not in v: #See whether the successor or child hasn't been visited
+                nw=list(current) #copy the previous path list into the new one.
+                path = i[1]
+                is_visted = i[0]
+                nw.append(path)
+                F.push((is_visted, nw)) # Insert the path and node onto the stack.
+
+    util.raiseNotDefined()
+
 def nullHeuristic(state, problem=None):
     """
     A heuristic function estimates the cost from the current state to the nearest
     goal in the provided SearchProblem.  This heuristic is trivial.
     """
     return 0
+
+# Abbreviations
+dfs = depthFirstSearch()
